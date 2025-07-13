c7dfe3e78b68cc0fdb8124f6be20b1dad444e7dc
Nienke <19731644+NienkeJ@users.noreply.github.com>
2025-07-13 14:21:37 +0200

    Update diceRoller.py
    Cap Hope and Fear trackers per TTRPG rules regulations.
    
    Add test to see if Hope or Fear can be used (no longer allowing negative numbers).
    
    Added a few comments, for ex. in line 25 and 31 to further document what blocks doe.
    
    Removed int() from lines 39 - 41, as user on StackOverflow pointed out that this is not necessary per [documentation](https://docs.python.org/3/library/random.html#random.randrange).

--- diceRoller.py
+++ diceRoller.py
@@ -19,27 +19,37 @@ while True: # The main game loop.
             if playerMove == 'q':
                 sys.exit() # Quit the program.
             if playerMove == 'r':
-                break # Break out of the player input loop
+                break # Break out of the player input loop and to 'roll the dice'.
             if playerMove == 'h':
-                hope = hope - 1
-                print('%s Hope, %s Fear' % (hope, fear))
+                if hope > 0:
+                    hope = hope - 1 # Allows player to use a Hope resource if available, lowering the tracker by one.
+                    print('%s Hope, %s Fear' % (hope, fear))
+                else:
+                    print('No hope available.')
             if playerMove == 'f':
-                fear = fear - 1
-                print('%s Hope, %s Fear' % (hope, fear))
+                if fear > 0:
+                    fear = fear - 1 # Allows solo player / GM to use a Fear resource if available, lowering tracker by one.
+                    print('%s Hope, %s Fear' % (hope, fear))
+                else:
+                    print('No fear available.')
             print('Choose adventurer, and (r)oll, (q)uit, use (h)ope or use (f)ear.')
 
         # Roll the dice
         if playerMove == 'r':
-            hopeDice = int(random.randint(1, 12))
-            fearDice = int(random.randint(1, 12))
+            hopeDice = random.randint(1, 12)
+            fearDice = random.randint(1, 12)
             roll = int(hopeDice) + int(fearDice)
             print('You rolled {} with ...'.format(roll))
             if hopeDice > fearDice:
                 print('... hope')
-                hope = hope + 1
+                if hope < 6: # The TTRPG rules allow you to have up to but not exeeding 6 Hope at any time.
+                    hope += 1
             if hopeDice < fearDice:
                 print('... fear')
-                fear = fear + 1
+                if fear < 6: # The TTRPG rules allow you to have up to but not exeeding 6 Fear at any time.
+                    fear += 1
             if hopeDice == fearDice:
-                print('Doubles! An extreme event')
+                print('Critical succes! You may clear one stress and possibly increase the Damage calculation (if applicable).')
+                if hope < 6:
+                    hope += 1
         print('%s Hope, %s Fear' % (hope, fear))
